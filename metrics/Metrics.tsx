d15cdd708ecebb5b51fb285c2f066a3e3322f59e
konyan <devkonyan@gmail.com>
2024-05-26 01:22:26 +0700

    fix and update

--- metrics/Metrics.tsx
+++ metrics/Metrics.tsx
@@ -1,11 +1,14 @@
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Umami from './Umami';
 
-const Metrics = () => (
-  <>
-    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_TRAKCING_ID as string} />
-    <Umami />
-  </>
-);
+const Metrics = () => {
+  console.log('asea', process.env.NEXT_PUBLIC_GA_TRAKCING_ID);
+  return (
+    <>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_TRAKCING_ID as string} />
+      <Umami />
+    </>
+  );
+};
 
 export default Metrics;
--- metrics/Umami.tsx
+++ metrics/Umami.tsx
@@ -1,15 +1,13 @@
-'use client';
-
 import Script from 'next/script';
 
 const Umami = () => {
   return (
     <Script
-      id="fb-pixel"
+      id="umami"
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
-(function() { var el = document.createElement('script'); el.setAttribute('src', 'https://umami.konyan.dev/script.js'); el.setAttribute('data-website-id', process.env.UMAMI_TRACKING_ID as string); document.body.appendChild(el); })();
+(function() { var el = document.createElement('script'); el.setAttribute('src', 'https://umami.konyan.dev/script.js'); el.setAttribute('data-website-id', '23b54e7d-4714-436f-9647-f1760484adca'); document.body.appendChild(el); })();
     `,
       }}
     />
