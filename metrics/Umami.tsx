14cda281b1bbe6fb5d272bfa1987c8227523660a
konyan <devkonyan@gmail.com>
2024-05-26 01:13:59 +0700

    fix

--- metrics/Umami.tsx
+++ metrics/Umami.tsx
@@ -3,16 +3,17 @@
 import Script from 'next/script';
 
 const Umami = () => {
-  return;
-  <Script
-    id="fb-pixel"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
+  return (
+    <Script
+      id="fb-pixel"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
 (function() { var el = document.createElement('script'); el.setAttribute('src', 'https://umami.konyan.dev/script.js'); el.setAttribute('data-website-id', process.env.UMAMI_TRACKING_ID as string); document.body.appendChild(el); })();
     `,
-    }}
-  />;
+      }}
+    />
+  );
 };
 
 export default Umami;
